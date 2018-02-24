5b02c12c52efe2815417b1625a1fa121af7312e3
JifuZhao <jifuzhao@gmail.com>
2018-02-23 22:14:25 -0600

    sync

--- utils.py
+++ utils.py
@@ -4,33 +4,92 @@
 __author__  =   "Jifu Zhao"
 __email__   =   "jzhao59@illinois.edu"
 __date__    =   "02/20/2018"
-__update__  =   "02/20/2018"
+__update__  =   "02/23/2018"
 """
 
 import pandas as pd
 import requests
-import datetime
 import calendar
 import time
 
-# key for wunderground.com API
-KEY = 'ac6a917d396d3bd0'
 
-
-def query_weather(start_year, end_year, key, state='IL', area='Chicago'):
+def query_weather(key, year, state='IL', area='Chicago'):
     """ Qeury the weather information from https://www.wunderground.com
-        at the given area in the given time range
+        at the given area in the given year (query frequency is limited)
     """
     # define url template to query weather
     prefix = 'https://api.wunderground.com/api/'
     url = prefix + '{key}/history_{year}{month}{day}/q/{state}/{area}.json'
 
     # lists to store the data
-    timeList = []
-    date = []
+    date_list = []
     temperature = []
+    wind_chill = []
     dewpoint = []
     humidity = []
-    precipitation = []
     pressure = []
+    visibility = []
+    wind_speed = []
+    precipitation = []
+    events = []
     rain = []
+    conditions = []
+
+    # query data for each month
+    for month in range(1, 2):
+        # get the number of days at the given month
+        num_days = calendar.monthrange(year, month)[1]
+
+        # transform into string
+        year_str = str(year)
+        month_str = str(month)
+        if month < 10:
+            month_str = '0' + str(month)
+
+        # query the weather for each day
+        for day in range(1, num_days + 1):
+            day_str = str(day)
+            if day < 10:
+                day_str = '0' + day_str
+
+            # query the weather information
+            link = url.format(key=key, year=year_str, month=month_str,
+                              day=day_str, state=state, area=area)
+            weather = requests.get(link).json()['history']['observations']
+
+            # add new data to list
+            for i in range(len(weather)):
+                date = weather[i]['date']['year'] + '-' \
+                    + weather[i]['date']['mon'] + '-' \
+                    + weather[i]['date']['mday'] + '-' \
+                    + weather[i]['date']['hour'] + ':' \
+                    + weather[i]['date']['min'] + ':00'
+                date_list.append(date)
+                temperature.append(weather[i]['tempi'])
+                wind_chill.append(weather[i]['windchilli'])
+                dewpoint.append(weather[i]['dewpti'])
+                humidity.append(weather[i]['hum'])
+                pressure.append(weather[i]['pressurei'])
+                visibility.append(weather[i]['visi'])
+                wind_speed.append(weather[i]['wspdi'])
+                precipitation.append(weather[i]['precipi'])
+                events.append(weather[i]['icon'])
+                rain.append(weather[i]['rain'])
+                conditions.append(weather[i]['conds'])
+
+            time.sleep(6)
+
+    # transform to DataFrame
+    Dict = {'date': date_list, 'temperature': temperature,
+            'windchill': wind_chill, 'dewpoint': dewpoint,
+            'humidity': humidity, 'pressure': pressure,
+            'visibility': visibility, 'wind_speed': wind_speed,
+            'precipitation': precipitation, 'events': events,
+            'rain': rain, 'conditions': conditions}
+
+    columns = ['date', 'temperature', 'windchill', 'dewpoint', 'humidity',
+               'pressure', 'visibility', 'wind_speed', 'precipitation',
+               'events', 'rain', 'conditions']
+    df = pd.DataFrame(data=Dict, columns=columns)
+
+    return df
